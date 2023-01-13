b4eab215a0ea72908b4d9955eea4fceecb28bfce
Emily Pisaygnane <episaygnane@me.com>
2023-01-12 17:08:31 -0800

    imported reducers, exported provider, and context

--- src/components/ListProvider.jsx
+++ src/components/ListProvider.jsx
@@ -1,1 +1,19 @@
-// import { createContext, useReducer } from 'react';
+import { createContext, useReducer } from 'react';
+import { initialState, reducer } from '../reducers/shopping-list-item-reducer';
+import { reduceLogger } from '../reducers/reduce-logger';
+
+export const Context = createContext({
+  state: initialState(),
+  reducer,
+});
+
+export const ListProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(
+    reduceLogger(reducer),
+    initialState(),
+  );
+  const contextObj = { state, dispatch };
+  return <Context.Provider value={ contextObj }>
+    { children }
+  </Context.Provider>;
+};
